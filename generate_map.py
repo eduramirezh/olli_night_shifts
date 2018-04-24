1922023d3ebf8e4bf2228951aebf125747150ccf
Eduardo Ramírez <ejramire@uc.cl>
2018-04-24 09:59:42 +0200

    23.04

--- generate_map.py
+++ generate_map.py
@@ -12,25 +12,24 @@ stations = Station.get_stations()
 lines = Line.get_lines()
 
 for station in stations:
-    if 'train' in station.modes():
-        folium.Marker(station.location, popup=station.name).add_to(marker_cluster)
+    folium.Marker(station.location, popup=station.name).add_to(marker_cluster)
 
-is_ubahn = False
-colors = ['red', 'green', 'yellow', 'black', 'pink', 'brown', 'red', 'green', 'yellow', 'black', 'pink', 'brown', 'red', 'green', 'yellow', 'black', 'pink', 'brown', 'red', 'green', 'yellow', 'black', 'pink', 'brown']
-for line in lines:
-    # if not line.mode == 'train':
-    if line.name != 'U8':
-        continue
-    color = 'green'
-    if line.product == 'subway':
-        color = 'blue'
-    multipolyline = []
-    i = 0
-    for variant in line.variants:
-        polyline = [station.location for station in variant]
-        multipolyline.append(polyline)
-        folium.features.PolyLine(polyline, color=colors[i]).add_to(m)
-        i += 1
-
+#is_ubahn = False
+#colors = ['red', 'green', 'yellow', 'black', 'pink', 'brown', 'red', 'green', 'yellow', 'black', 'pink', 'brown', 'red', 'green', 'yellow', 'black', 'pink', 'brown', 'red', 'green', 'yellow', 'black', 'pink', 'brown']
+#for line in lines:
+#    # if not line.mode == 'train':
+#    if line.name != 'U8':
+#        continue
+#    color = 'green'
+#    if line.product == 'subway':
+#        color = 'blue'
+#    multipolyline = []
+#    i = 0
+#    for variant in line.variants:
+#        polyline = [station.location for station in variant]
+#        multipolyline.append(polyline)
+#        folium.features.PolyLine(polyline, color=colors[i]).add_to(m)
+#        i += 1
+#
 
 m.save(os.path.join('results', 'stopsandlines.html'))
--- models/station.py
+++ models/station.py
@@ -1,4 +1,6 @@
 import requests
+import dateparser
+import json
 SHUTTLE_SPEED = 30 #km/hr
 
 class Stop():
@@ -45,6 +47,18 @@ class Station():
         """tbd"""
         pass
 
+    def departures(self, timestamp):
+        """tbd"""
+        journeys = []
+        url = f'http://localhost:3000/stations/{self._id}/departures?when={int(timestamp)}'
+        journeys_data = requests.get(url).json()
+        for journey in journeys_data:
+            if journey.get('cancelled', False):
+                continue
+            journeys.append(Journey(journey['journeyId'], self, journey['when'], journey['direction'], journey['line'], journey['trip'], journey['delay']))
+        return journeys
+
+
     @classmethod
     def common_linked_stations(cls, station_a, station_b):
         """to be documented"""
@@ -90,8 +104,102 @@ class Station():
             as_dictionary[key] = station
         return as_list
 
+    @classmethod
+    def find_by_name(cls, name):
+        for station_id in cls.all_stations:
+            station = cls.all_stations[station_id]
+            if station.name == name:
+                return station
+        return None
+
     def modes(self):
         modes_set = set()
         for line in self.lines:
             modes_set.add(line.mode)
         return modes_set
+
+
+class Journey():
+    '''doc'''
+    def __init__(self, _id, station, when, direction, line, trip, delay):
+        self._id = _id
+        self.station = station
+        self.when = dateparser.parse(when)
+        self.direction = Station.find_by_name(direction)
+        self.line = Line.find_by_name(line['name'])
+        self.trip = trip
+        self.delay = delay
+
+
+class Line():
+    '''doc'''
+    all_lines = {}
+    def __init__(self, _id, name, operator, mode, product, variants):
+        self._id = _id
+        type(self).all_lines[_id] = self
+        self.name = name
+        self.operator = operator
+        self.mode = mode
+        self.product = product
+        self.variants = []
+        self.stations_set = set()
+        self.stops_set = set()
+        for variant in variants:
+            new_variant = []
+            for stop_id in variant:
+                stop = None
+                try:
+                    stop = Stop.all_stops[stop_id]
+                    self.stops_set.add(stop)
+                    self.stations_set.add(stop.station)
+                    stop.station.lines.add(self)
+                except:
+                    stop = Station.all_stations[stop_id]
+                    self.stations_set.add(stop)
+                stop.lines.add(self)
+                new_variant.append(stop)
+            self.variants.append(new_variant)
+
+
+    @classmethod
+    def get_lines(cls, return_dictionary=False):
+        '''doc'''
+        lines_data = requests.get('http://localhost:3000/lines?variants=true')
+        as_dictionary = {}
+        as_list = []
+        for line_data in lines_data.text.split('\n'):
+            if len(line_data) < 2:
+                continue
+            data = json.loads(line_data)
+            line = cls(data['id'], data['name'], data['operator'], data['mode'],
+                       data['product'], data['variants'])
+            as_list.append(line)
+            as_dictionary[data['id']] = line
+        if return_dictionary:
+            return as_dictionary
+        return as_list
+
+
+    def contains_stop_or_station(self, stop_or_station):
+        return stop_or_station in self.stations_set
+
+
+    def intersecting_lines(self):
+        all_lines = type(self).all_lines
+        intersecting_lines = {}
+        for line_id in all_lines:
+            if line_id == self._id:
+                continue
+            line = all_lines[line_id]
+            common_stations = line.stations_set.intersection(self.stations_set)
+            if len(common_stations) > 0:
+                intersecting_lines[line._id] = common_stations
+        return intersecting_lines
+
+    @classmethod
+    def find_by_name(cls, name):
+        for line_id in cls.all_lines:
+            line = cls.all_lines[line_id]
+            if line.name == name:
+                return line
+        return None
